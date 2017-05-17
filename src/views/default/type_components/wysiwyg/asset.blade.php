2bce64d79191e4f95b1b6a7200b8ccc7734d7460
Benjamin Schnabel <Benjamin-777@gmx.de>
2017-05-17 15:52:40 +0200

    :fire: Summernote replaced with Tinymce 4.5.7

--- src/views/default/type_components/wysiwyg/asset.blade.php
+++ src/views/default/type_components/wysiwyg/asset.blade.php
@@ -1,2 +1,1 @@
-<link rel="stylesheet" type="text/css" href="{{asset('vendor/crudbooster/assets/summernote/summernote.css')}}">
-<script type="text/javascript" src="{{asset('vendor/crudbooster/assets/summernote/summernote.js')}}"></script>  
+<script type="text/javascript" src="{{asset('vendor/crudbooster/assets/tinymce/tinymce.min.js')}}"></script>  
